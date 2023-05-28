ed4c19bea1750033c7b7c9e865ac280b7e155195
Thanh Le <lenguyenthanh@hotmail.com>
2023-05-28 21:49:55 +0200

    Done circe.Codec derivation

--- core/src/main/scala/puzzle/Domain.scala
+++ core/src/main/scala/puzzle/Domain.scala
@@ -4,7 +4,7 @@ import eu.timepit.refined.types.string.NonEmptyString
 import eu.timepit.refined.cats.*
 import io.circe.refined.*
 
-import cats.{ Eq, Show }
+import cats.{Eq, Show}
 import cats.derived.*
 import cats.syntax.all.*
 import io.circe.Codec
@@ -14,17 +14,19 @@ import chess.format.*
 
 import OrphanInstances.given
 
-// case class Puzzle(
-//   id: PuzzleId,
-//   fen: EpdFen,
-//   moves: List[Uci],
-//   rating: Int,
-//   ratingDeviation: Int,
-//   popularity: Int,
-//   nbPlays: Int,
-//   themes: List[NonEmptyString],
-//   openingTags: List[NonEmptyString],
-// ) derives Codec.AsObject, Eq, Show
+case class Puzzle(
+  id: PuzzleId,
+  fen: EpdFen,
+  moves: List[Uci],
+  rating: Int,
+  ratingDeviation: Int,
+  popularity: Int,
+  nbPlays: Int,
+  themes: List[NonEmptyString],
+  openingTags: List[NonEmptyString],
+) derives Codec.AsObject,
+    Eq,
+    Show
 
 case class User(id: UserId, name: NonEmptyString) derives Codec.AsObject, Eq, Show
 
