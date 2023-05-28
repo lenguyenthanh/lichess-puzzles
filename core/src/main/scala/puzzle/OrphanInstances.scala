4d99e3a9ffa9dc1604791ab1b8128d9afa9872e6
Thanh Le <lenguyenthanh@hotmail.com>
2023-05-28 21:49:55 +0200

    Done circe.Codec derivation

--- core/src/main/scala/puzzle/OrphanInstances.scala
+++ core/src/main/scala/puzzle/OrphanInstances.scala
@@ -1,16 +1,28 @@
 package puzzle
 
-import cats.{Eq, Order, Show}
+import cats.{Eq, Show}
 
-import chess.format.Uci
-import io.circe.Codec
+import chess.format.{EpdFen, Uci}
+import io.circe.Encoder
+import io.circe.Decoder
+import io.circe.DecodingFailure
 
 object OrphanInstances:
 
-  given Eq[Uci]    = Eq.by(_.uci)
-  given Show[Uci]  = Show.show(_.uci)
+  given Eq[Uci] = Eq.by(_.uci)
+  given Show[Uci] = Show.show(_.uci)
 
-  given Eq[EpdFen]    = Eq.by(_.value)
-  given Show[EpdFen]    = Eq.by(_.value)
+  given Eq[EpdFen] = Eq.by[EpdFen, String](_.value)
+  given Show[EpdFen] = Show.show[EpdFen](_.value)
 
-  // given Codec[Uci] = Codec.from(Codec[String], Codec[String].map(Uci.apply))
+  given Encoder[Uci] = Encoder[String].contramap(_.uci)
+
+  given Decoder[Uci] = Decoder.instance: c =>
+    summon[Decoder[String]](c) match
+      case Right(t0) => Uci(t0) match
+          case None        => Left(DecodingFailure(s"not an uci $t0", c.history))
+          case r @ Some(t) => Right(t)
+      case l @ Left(_) => l.asInstanceOf[Decoder.Result[Uci]]
+
+  given Encoder[EpdFen] = Encoder[String].contramap(_.value)
+  given Decoder[EpdFen] = Decoder[String].map(EpdFen(_))
