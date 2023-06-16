54314f9d2979c7c87ca02b16341afab31d1f0048
Thanh Le <lenguyenthanh@hotmail.com>
2023-06-16 21:29:46 +0200

    Add query byName for Themes

---
+++ modules/database/src/main/scala/puzzle/db/PuzzleThemes.scala
@@ -0,0 +1,38 @@
+package puzzle
+package db
+
+import scala.util.control.NoStackTrace
+import cats.syntax.all.*
+import cats.effect.kernel.Resource
+import cats.effect.MonadCancelThrow
+
+import skunk.*
+import skunk.implicits.*
+
+import Codecs.*
+
+trait PuzzleThemes[F[_]]:
+  def upsert(puzzleId: PuzzleId, themeId: ThemeId): F[Unit]
+
+case class PuzzleIdExists(id: PuzzleId) extends NoStackTrace
+
+object PuzzleThemes:
+  def apply[F[_]: MonadCancelThrow](postgres: Resource[F, Session[F]]): PuzzleThemes[F] = new:
+    import PuzzleThemesSQL.*
+
+    def upsert(puzzleId: PuzzleId, themeId: ThemeId) =
+      postgres.use: session =>
+        session.prepare(insert).flatMap: cmd =>
+          cmd.execute(puzzleId, themeId).void
+        .recoverWith:
+          case SqlState.UniqueViolation(_) =>
+            PuzzleIdExists(puzzleId).raiseError
+
+
+
+private object PuzzleThemesSQL:
+  val insert: Command[(PuzzleId, ThemeId)] =
+    sql"""
+        INSERT INTO puzzle_themes (puzzle_id, theme_id)
+        VALUES ($puzzleId, $themeId)
+       """.command
--- modules/database/src/test/scala/puzzle/db/ThemesSuite.scala
+++ modules/database/src/test/scala/puzzle/db/ThemesSuite.scala
@@ -4,11 +4,13 @@ package tests
 
 import weaver.*
 import eu.timepit.refined.types.string.NonEmptyString
+import cats.kernel.Eq
 
 object ThemesSuite extends SimpleIOSuite:
 
-  val name1 = NonEmptyString.unsafeFrom("test1")
-  val name2 = NonEmptyString.unsafeFrom("test2")
+  given Eq[NonEmptyString] = Eq.fromUniversalEquals
+  val name1                = NonEmptyString.unsafeFrom("test1")
+  val name2                = NonEmptyString.unsafeFrom("test2")
 
   private def resource =
     Fixture.createRepositoryResource.map(_.themes)
@@ -20,10 +22,18 @@ object ThemesSuite extends SimpleIOSuite:
   test("insert a theme twice throw ThemExists"):
     resource.use: themes =>
       themes.create(name1) >> themes.create(name1).attempt.map:
-        case Left(ThemeExists(_)) => expect(true)
+        case Left(ThemeExists(x)) => expect.eql(x, name1)
         case _                    => expect(false)
 
-  test("insert a two theme andThen get"):
+  test("insert.get == Some"):
+    resource.use: themes =>
+      for
+        _ <- themes.create(name1)
+        o <- themes.byName(name1)
+      yield matches(o):
+        case Some(x) => expect.eql(x.name, name1)
+
+  test("insert a two theme andThen list"):
     resource.use: themes =>
       for
         _      <- themes.create(name1)
